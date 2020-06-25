c4264d01de6bbaa210ed0c6adc7403e30625bffb
Francesco Sullo <sullof@gmail.com>
2020-06-25 09:16:10 -0700

    Temporary commit

--- bin/check-versions-and-patch.sh
+++ bin/check-versions-and-patch.sh
@@ -3,16 +3,17 @@
 VERCORE=0
 VERFS=0
 VERSECREZ=0
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-DIFF=$(git diff packages/core)
+DIFF=$(git diff master..$BRANCH packages/core)
 if [[ "$DIFF" != "" ]]; then
   VERCORE=$(npm view @secrez/core | grep latest)
 fi
-DIFF=$(git diff packages/fs)
+DIFF=$(git diff master..$BRANCH packages/fs)
 if [[ "$DIFF" != "" ]]; then
   VERFS=$(npm view @secrez/fs | grep latest)
 fi
-DIFF=$(git diff packages/secrez)
+DIFF=$(git diff master..$BRANCH packages/secrez)
 if [[ "$DIFF" != "" ]]; then
   VERSECREZ=$(npm view secrez | grep latest)
 fi
