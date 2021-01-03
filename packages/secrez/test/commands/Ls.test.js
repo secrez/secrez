8a6a0d91e142447e711f322e13f1d1fbc20010a4
Francesco Sullo <sullof@gmail.com>
2021-01-02 19:57:32 -0800

    Adds attributes to ls -l

--- packages/secrez/test/commands/Ls.test.js
+++ packages/secrez/test/commands/Ls.test.js
@@ -1,5 +1,6 @@
 const chai = require('chai')
 const assert = chai.assert
+const _ = require('lodash')
 const stdout = require('test-console').stdout
 
 const fs = require('fs-extra')
@@ -12,6 +13,7 @@ const {
   iterations
 } = require('../fixtures')
 
+
 describe('#Ls', function () {
 
   let prompt
@@ -23,6 +25,18 @@ describe('#Ls', function () {
     localDir: __dirname
   }
 
+  let when
+
+  function formatResult(r) {
+    return [
+      0,
+      when,
+      when,
+      1,
+      r
+    ].join('  ')
+  }
+
   beforeEach(async function () {
     await fs.emptyDir(path.resolve(__dirname, '../../tmp/test'))
     prompt = new MainPrompt
@@ -30,7 +44,7 @@ describe('#Ls', function () {
     C = prompt.commands
     await prompt.secrez.signup(password, iterations)
     await prompt.internalFs.init()
-
+    when = C.ls.formatTs(Date.now())
   })
 
   it('should return the help', async function () {
@@ -69,12 +83,24 @@ describe('#Ls', function () {
     inspect = stdout.inspect()
     await C.ls.exec({path: '/dir1', list: true})
     inspect.restore()
-    assertConsole(inspect, ['dir2A/', 'dir2B/', 'dirA1/', 'dirA2/', 'dirB1/', 'dirB2/', 'file1', 'file2'])
+    assertConsole(inspect, [
+      formatResult('dir2A/'),
+      formatResult('dir2B/'),
+      formatResult('dirA1/'),
+      formatResult('dirA2/'),
+      formatResult('dirB1/'),
+      formatResult('dirB2/'),
+      formatResult('file1'),
+      formatResult('file2')
+    ])
 
     inspect = stdout.inspect()
     await C.ls.exec({path: '/dir1/dir2A', list: true})
     inspect.restore()
-    assertConsole(inspect, ['dir6/', 'dir7/'])
+    assertConsole(inspect, [
+      formatResult('dir6/'),
+      formatResult('dir7/')
+    ])
 
     inspect = stdout.inspect()
     await C.ls.exec({path: '/dir1/dir2B', list: true})
@@ -119,22 +145,37 @@ describe('#Ls', function () {
     inspect = stdout.inspect()
     await C.ls.exec({path: '/dir1/d?rB?', list: true})
     inspect.restore()
-    assertConsole(inspect, ['dirB1/', 'dirB2/', 'dorB3/'])
+    assertConsole(inspect, [
+      formatResult('dirB1/'),
+      formatResult('dirB2/'),
+      formatResult('dorB3/')
+    ])
 
     inspect = stdout.inspect()
     await C.ls.exec({path: '/dir1/dir*', list: true})
     inspect.restore()
-    assertConsole(inspect, ['dir2A/', 'dir2B/', 'dirA1/', 'dirA2/', 'dirB1/', 'dirB2/'])
+    assertConsole(inspect, [
+      formatResult('dir2A/'),
+      formatResult('dir2B/'),
+      formatResult('dirA1/'),
+      formatResult('dirA2/'),
+      formatResult('dirB1/'),
+      formatResult('dirB2/')])
 
     inspect = stdout.inspect()
     await C.ls.exec({path: '/dir1/dir?1', list: true})
     inspect.restore()
-    assertConsole(inspect, ['dirA1/', 'dirB1/'])
+    assertConsole(inspect, [
+      formatResult('dirA1/'),
+      formatResult('dirB1/')])
 
     inspect = stdout.inspect()
-    await C.ls.exec({path: '/dir1/dir[AB]*', list: true})
+    await C.ls.exec({path: '/dir1/dir[AB]*'})
     inspect.restore()
-    assertConsole(inspect, ['dirA1/', 'dirA2/', 'dirB1/', 'dirB2/'])
+    assert.equal(
+        _.trim(decolorize(inspect.output).split(/ +/).map(e => _.trim(e)).join(' ')),
+        'dirA1/ dirA2/ dirB1/ dirB2/'
+    )
 
   })
 
